820a4ec12e6eb01022cb5487d0ece7b1bb4403b8
Karen Cardoso <karencscardoso@gmail.com>
2022-08-10 17:02:34 -0300

    Remove border
    Removi as bordas de teste que havia feito.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'category', component: CategoryComponent}
+  {path: 'category', component: CategoryComponent},
 ];
 
 @NgModule({
